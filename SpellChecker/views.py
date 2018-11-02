d5cdf106876167482ea43f03fe2a85b05a1f5c09
Denis Petelin <d.petelin@icloud.com>
2018-11-02 13:13:14 -0400

    Basic file upload handler.

--- SpellChecker/views.py
+++ SpellChecker/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+from SpellChecker.forms import DocumentForm
+
 
 def welcome(request):
     return render(request, 'welcome.html')
@@ -12,3 +14,16 @@ def home(request):
         return render(request, 'files.html')
     else:
         return redirect('welcome')
+
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'model_form_upload.html', {
+        'form': form
+    })
