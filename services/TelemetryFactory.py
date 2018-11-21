0ef16f8e22d7918c4aba1d71a0db9cbcb1f004dc
Denis Petelin <d.petelin@icloud.com>
2018-11-20 20:32:16 -0500

    TelemetryFactory now is able to produce sessions and clients for the AWS Logs

--- services/TelemetryFactory.py
+++ services/TelemetryFactory.py
@@ -1,18 +1,31 @@
 import os
 import logging, watchtower
 
-from boto3 import Session
+from boto3 import Session, client
 
+AWS_REGION_NAME = 'us-east-1'
+AWS_ACCESS_KEY_ID = os.environ.get('ACCESS_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('ACCESS_KEY')
 
 def create(name):
     logging.basicConfig(level=logging.INFO)
-    AWS_ACCESS_KEY_ID = os.environ.get('ACCESS_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('ACCESS_KEY')
-    AWS_REGION_NAME = 'us-east-1'
+    boto3_session = get_aws_session()
+    logger = logging.getLogger(name=name)
+    logger.addHandler(
+        watchtower.CloudWatchLogHandler(log_group='Typonator', stream_name='TyponatorLog', boto3_session=boto3_session))
+    return logger
+
+
+def get_aws_session():
 
     boto3_session = Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
                             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
                             region_name=AWS_REGION_NAME)
-    logger = logging.getLogger(name=name)
-    logger.addHandler(watchtower.CloudWatchLogHandler(log_group='Typonator', stream_name='TyponatorLog', boto3_session=boto3_session))
-    return logger
+    return boto3_session
+
+
+def get_aws_client():
+    boto3_client = client(aws_access_key_id=AWS_ACCESS_KEY_ID,
+                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                          region_name=AWS_REGION_NAME)
+    return boto3_client
